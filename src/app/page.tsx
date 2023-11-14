c8f3eb2445d0d7cb2521c9a22df3402b9437b193
Nicolino Ayerdi <nicolino.ayerdi@sifted.com>
2023-11-14 08:28:22 -0300

    redirect to /routines on load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-	return (
-		<div>
-			<div>Feed</div>
-			<div>Charts</div>
-			<div>Summary of data</div>
-		</div>
-	);
+	redirect('/routines');
 }
